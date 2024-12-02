39e78f0ebc5b0fd123d949c9dd1e3429653b7567
Null <139032361+0NullReference0@users.noreply.github.com>
2024-12-02 14:59:55 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert('test');
+});
+primaryButton.addEventListener('discord', function() {
+    open("https://discord.gg/EpfaHPrAsH")
+});
